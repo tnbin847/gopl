2cdcd1ba0c3c5c10f3edd47d8b5957d2fe1f4d81
tnbin847 <tnqls3718@gmail.com>
2024-12-06 17:55:12 +0900

    Initial commit

---
+++ src/main/java/com/bins/gopl/GoplApplication.java
@@ -0,0 +1,13 @@
+package com.bins.gopl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bins/gopl/GoplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bins.gopl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
